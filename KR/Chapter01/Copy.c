2808f0eba1b6e5d5cb37d4607f6d8fb4dad15586
Lilac Walia <lavenderbonnet@outlook.com>
2020-12-21 17:22:13 -0600

    copy in out

---
+++ KR/Chapter01/Copy.c
@@ -0,0 +1,15 @@
+// Lilac Walia | Dec 21, 2020
+
+#include <stdio.h>
+
+main()
+{
+    int c;
+
+    c = getchar();
+    while (c != EOF)
+    {
+        putchar(c);
+        c = getchar();
+    }
+}
