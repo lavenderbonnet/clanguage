15bdd0d110ec38332123e228768ffe8e57412c3a
Lilac Walia <lavenderbonnet@outlook.com>
2020-12-21 16:42:47 -0600

    Alphabet Play

---
+++ KR/Chapter01/Alphabet.c
@@ -0,0 +1,47 @@
+// Lilac Walia | Dec 21, 2020
+
+#include <stdio.h>
+
+//Printing the entire table
+void PrintCharTable()
+{
+    char c = 'a';
+
+    int i = 1;
+
+    while( i <= 256)
+    {
+        putchar(c);
+        c = c + 1;
+
+        i = i + 1;
+    }
+    
+    printf("\n\n");
+    
+    return;
+}
+
+void Print2z()
+{
+    char c;
+
+    printf("Please enter a lower case letter from the alphabet.\n\n");
+
+    c = getchar();
+    while(c != '{')
+    {
+        putchar(c);
+        c = c + 1;
+    }
+
+    return;
+}
+
+int main()
+{
+    PrintCharTable();
+    Print2z();
+
+    return 1;
+}
---
+++ KR/Chapter01/BasicInOut.c
@@ -0,0 +1,16 @@
+// Lilac Walia | Dec 21, 2020
+
+#include <stdio.h>
+
+int main()
+{
+    char c;
+    while(c != 'q')
+    {
+        c = getchar();
+
+        putchar(c);
+    }
+
+    return 1;
+}
